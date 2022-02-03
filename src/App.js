015c9e3817430e6d61de6a69b23464f97a5f6956
Alex Pritchard <alexbeet1234@gmail.com>
2022-02-03 19:27:25 +0800

    added usestate to test toggle background color

--- src/App.js
+++ src/App.js
@@ -4,11 +4,15 @@ import { useState } from 'react';
 function App() {
   const [isDarkMode, setIsDarkMode] = useState(true);
 
+  const darkModeHandler = () => {
+    setIsDarkMode((prevState) => !prevState)
+  }
+
   const backgroundClasses = isDarkMode ? classes.backgroundDark : classes.backgroundLight
 
   return (
     <div className={`${classes.background} ${backgroundClasses}`}>
-      <button>Click me</button>
+      <button onClick={darkModeHandler}>Click me</button>
     </div>
   );
 }
