06700c83dfb9565731457ab10a2e82c6115d656f
Alex Pritchard <alexbeet1234@gmail.com>
2022-02-04 10:51:19 +0800

    added dark light mode to navbar

--- src/Components/UI/NavBar.js
+++ src/Components/UI/NavBar.js
@@ -8,18 +8,22 @@ import classes from "./NavBar.module.css";
 const NavBar = () => {
   const darkLightCtx = useContext(DarkLightContext);
 
+  const isDarkMode = darkLightCtx.isDarkMode;
+
   const darkLightIconHandler = (event) => {
     event.preventDefault();
 
     darkLightCtx.toggleDarkLight();
   }
 
+  const navBarDarkLightClasses = isDarkMode ? classes.navbarDark : classes.navbarLight
+
   return (
     <Fragment>
       <div onClick={darkLightIconHandler} className={classes.darkLightModeIcon}>
           <MdDarkMode />
       </div>
-      <div className={`${classes.navbar} ${classes.navbarDark}`}>
+      <div className={`${classes.navbar} ${navBarDarkLightClasses}`}>
         <a href="#home">Home</a>
         <a href="#work">Work</a>
         <a href="#contact">Contact</a>
