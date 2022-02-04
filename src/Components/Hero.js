dbf9c60ba405209b320dc59b5803194dd3f8ec20
Alex Pritchard <alexbeet1234@gmail.com>
2022-02-04 16:10:18 +0800

    added grid to hero container;

--- src/Components/Hero.js
+++ src/Components/Hero.js
@@ -1,9 +1,19 @@
+import { useContext } from "react";
+
 import classes from "./Hero.module.css";
 import heroDark from '../images/hero-dark.png';
+import heroLight from '../images/hero-light.png';
+import DarkLightContext from "../store/dark-light-context";
 
 const Hero = () => {
+  const darkLightCtx = useContext(DarkLightContext)
+  const isDarkMode = darkLightCtx.isDarkMode;
+
+  const heroImg = isDarkMode ? heroDark : heroLight;
+  const heroSectionClasses = isDarkMode ? classes.heroSectionDarkMode : classes.heroSectionLightMode;
+
   return (
-    <section className={classes.heroSection}>
+    <section className={`${classes.heroSection} ${heroSectionClasses}`}>
       <div>
         <h1>Hello, I'm</h1>
         <h2>Alex Pritchard</h2>
@@ -14,11 +24,11 @@ const Hero = () => {
           minim veniam, quis nostrud exercitation Lorem ipsum dolor sit amet,
           consectetur adipiscing elit, sed do eiusmod tempor incididunt ut
           labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud
-          exercitation
+          <a href="#"> exercitation</a>
         </p>
       </div>
-      <div>
-        {/* <img src={heroDark} alt="hero-image"></img> */}
+      <div className={classes.image}>
+        <img src={heroImg} alt="hero-image"></img>
       </div>
     </section>
   );
