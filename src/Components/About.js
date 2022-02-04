cd1c15f9baeab6c40070dce871f41e5115b56788
Alex Pritchard <alexbeet1234@gmail.com>
2022-02-04 16:50:57 +0800

    added profile photo to images folder

---
+++ src/Components/About.js
@@ -0,0 +1,36 @@
+import { useContext } from "react";
+
+import profilePhoto from '../images/profile-photo.png';
+import DarkLightContext from "../store/dark-light-context";
+import classes from "./About.module.css";
+
+const About = () => {
+  const darkLightCtx = useContext(DarkLightContext);
+
+  const isDarkMode = darkLightCtx.isDarkMode;
+
+  const aboutSectionClasses = isDarkMode
+    ? classes.aboutSectionDarkMode
+    : classes.aboutSectionLightMode;
+
+  return (
+    <section className={`${classes.aboutSection} ${aboutSectionClasses}`}>
+      <div className={classes.image}>
+        <img src={profilePhoto} alt="profile photo"/>
+      </div>
+      <div>
+        <h1>About Me</h1>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+          eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
+          minim veniam, quis nostrud exercitation Lorem ipsum dolor sit amet,
+          consectetur adipiscing elit, sed do eiusmod tempor incididunt ut
+          labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud
+          exercitation
+        </p>
+      </div>
+    </section>
+  );
+};
+
+export default About;
