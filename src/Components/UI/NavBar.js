412747a705085fbb0dc29ab2b162ed4e739ecfd5
Alex Pritchard <alexbeet1234@gmail.com>
2022-02-04 09:40:34 +0800

    created a links in navbar and styled for darkmode

--- src/Components/UI/NavBar.js
+++ src/Components/UI/NavBar.js
@@ -1,7 +1,18 @@
-import classes from './NavBar.module.css'
+import { Fragment } from "react/cjs/react.production.min";
+import classes from "./NavBar.module.css";
 
 const NavBar = () => {
-  return <div className={`${classes.navbar} ${classes.navbarDark}`}></div>
+  return (
+    <Fragment>
+      <h1 className={classes.test}>hello</h1>
+      <div className={`${classes.navbar} ${classes.navbarDark}`}>
+        <a href="#home">Home</a>
+        <a href="#work">Work</a>
+        <a href="#contact">Contact</a>
+        <a href="#resume">Resume</a>
+      </div>
+    </Fragment>
+  );
 };
 
 export default NavBar;
