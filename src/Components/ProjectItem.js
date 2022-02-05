b15b8eef30123f3289cdd430743d618da37e5085
Alex Pritchard <alexbeet1234@gmail.com>
2022-02-05 10:43:38 +0800

    created added responsive image

--- src/Components/ProjectItem.js
+++ src/Components/ProjectItem.js
@@ -4,13 +4,13 @@ import classes from "./ProjectItem.module.css";
 
 const ProjectItem = (props) => {
   return (
-    <div className={classes.projectContainer}>
+    <div className={`${classes.projectContainer} ${classes.projectContainerDarkMode}`}>
       <div className={classes.image}>
         <img src="https://images.unsplash.com/photo-1643960987290-37971bf5f2bd?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1171&q=80" alt="project" />
       </div>
       <div className={classes.projectContent}>
         <div className={classes.titleAndIcons}>
-          <h1>Project Title</h1>
+          <h2>Project Title</h2>
           <div className={classes.icons}>
             <AiFillGithub />
           </div>
