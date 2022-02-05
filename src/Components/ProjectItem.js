e3341a019ebb8752bd5bfbc794061ae71d991a70
Alex Pritchard <alexbeet1234@gmail.com>
2022-02-05 12:40:18 +0800

    set up projects from projectList using props

--- src/Components/ProjectItem.js
+++ src/Components/ProjectItem.js
@@ -22,32 +22,28 @@ const ProjectItem = (props) => {
     <div className={`${classes.projectContainer} ${projectClasses}`}>
       <div className={classes.image}>
         <img
-          src="https://images.unsplash.com/photo-1643960987290-37971bf5f2bd?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1171&q=80"
-          alt="image of webpage"
+          src={props.image}
+          alt={`screenshot of ${props.title}`}
         />
       </div>
       <div className={classes.projectContent}>
         <div className={classes.titleAndIcons}>
-          <h2>Project Title</h2>
+          <h2>{props.title}</h2>
           <div className={classes.icons}>
-            <a href="https://github.com/alexbeet81" target="_blank">
+            <a href={props.projectLink} target="_blank">
               <BiLinkExternal />
             </a>
-            <a href="https://github.com/alexbeet81" target="_blank">
+            <a href={props.gitRepo} target="_blank">
               <AiFillGithub />
             </a>
           </div>
         </div>
         <p>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-          eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
-          minim veniam, quis nostrud exercitation Lorem ipsum dolor sit amet,
-          consectetur adipiscing elit, sed do eiusmod tempor made using React,
-          Node.js and Djungo
+          {props.description}
         </p>
         <div className={classes.projectContentFooter}>
           <p>
-            Made using <a className={techUsedClasses}>React and Firebase</a>
+            Made using <a className={techUsedClasses}>{props.tech}</a>
           </p>
         </div>
       </div>
--- src/Components/Projects.js
+++ src/Components/Projects.js
@@ -4,6 +4,31 @@ import DarkLightContext from "../store/dark-light-context";
 import ProjectItem from "./ProjectItem";
 import classes from "./Projects.module.css";
 
+const PROJECTS = [
+  {
+    id: "p1",
+    title: "Project Title",
+    image:
+      "https://images.unsplash.com/photo-1643960987290-37971bf5f2bd?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1171&q=80",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor made using React, Node.js and Djungo",
+    tech: "React and Firebase",
+    projectLink: "#",
+    gitRepo: "https://github.com/alexbeet81",
+  },
+  {
+    id: "p2",
+    title: "Free Me",
+    image:
+      "https://images.unsplash.com/photo-1643960987290-37971bf5f2bd?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1171&q=80",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor made using React, Node.js and Djungo",
+    tech: "Ruby, Ruby on Rails and Heroku",
+    projectLink: "#",
+    gitRepo: "https://github.com/alexbeet81",
+  },
+];
+
 const Projects = () => {
   const darkLightCtx = useContext(DarkLightContext);
 
@@ -12,14 +37,25 @@ const Projects = () => {
   const projectSectionClasses = isDarkMode
     ? classes.projectsSectionDarkMode
     : classes.projectSectionLightMode;
-    
+
+  const projectList = PROJECTS.map(project => {
+    return(
+      <ProjectItem 
+        id={project.id}
+        title={project.title}
+        image={project.image}
+        description={project.description}
+        tech={project.tech}
+        projectLink={project.projectLink}
+        gitReop={project.gitRepo}
+      />
+    )
+  })
+
   return (
     <section className={`${classes.projectsSection} ${projectSectionClasses}`}>
       <h1>A Few Projects I've Built</h1>
-      <ProjectItem />
-      <ProjectItem />
-      <ProjectItem />
-      <ProjectItem />
+      {projectList}
     </section>
   );
 };
