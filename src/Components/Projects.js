e70993ef578f341bc8ccde2ce9ddf767f9ff789e
Alex Pritchard <alexbeet1234@gmail.com>
2022-02-05 12:50:20 +0800

    change margins on sections

--- src/Components/Projects.js
+++ src/Components/Projects.js
@@ -7,7 +7,7 @@ import classes from "./Projects.module.css";
 const PROJECTS = [
   {
     id: "p1",
-    title: "Project Title",
+    title: "Beet Blog",
     image:
       "https://images.unsplash.com/photo-1643960987290-37971bf5f2bd?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1171&q=80",
     description:
@@ -19,6 +19,28 @@ const PROJECTS = [
   {
     id: "p2",
     title: "Free Me",
+    image:
+      "https://images.unsplash.com/photo-1643960987290-37971bf5f2bd?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1171&q=80",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor made using React, Node.js and Djungo",
+    tech: "Ruby, Ruby on Rails and Heroku",
+    projectLink: "#",
+    gitRepo: "https://github.com/alexbeet81",
+  },
+  {
+    id: "p3",
+    title: "Classicar",
+    image:
+      "https://images.unsplash.com/photo-1643960987290-37971bf5f2bd?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1171&q=80",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor made using React, Node.js and Djungo",
+    tech: "Ruby, Ruby on Rails and Heroku",
+    projectLink: "#",
+    gitRepo: "https://github.com/alexbeet81",
+  },
+  {
+    id: "p4",
+    title: "Check It Off",
     image:
       "https://images.unsplash.com/photo-1643960987290-37971bf5f2bd?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1171&q=80",
     description:
