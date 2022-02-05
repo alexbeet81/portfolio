59c334f7b454b40544d620e1976f6792e95bd5d5
Alex Pritchard <alexbeet1234@gmail.com>
2022-02-05 11:15:21 +0800

    added h3 to project item

--- src/Components/ProjectItem.js
+++ src/Components/ProjectItem.js
@@ -1,12 +1,27 @@
 import { AiFillGithub } from "react-icons/ai";
+import { useContext } from "react";
 
 import classes from "./ProjectItem.module.css";
+import DarkLightContext from "../store/dark-light-context";
 
 const ProjectItem = (props) => {
+  const darkLightCtx = useContext(DarkLightContext);
+
+  const isDarkMode = darkLightCtx.isDarkMode;
+
+  const projectClasses = isDarkMode
+    ? classes.projectContainerDarkMode
+    : classes.projectContainerLightMode;
+
   return (
-    <div className={`${classes.projectContainer} ${classes.projectContainerDarkMode}`}>
+    <div
+      className={`${classes.projectContainer} ${projectClasses}`}
+    >
       <div className={classes.image}>
-        <img src="https://images.unsplash.com/photo-1643960987290-37971bf5f2bd?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1171&q=80" alt="project" />
+        <img
+          src="https://images.unsplash.com/photo-1643960987290-37971bf5f2bd?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1171&q=80"
+          alt="project"
+        />
       </div>
       <div className={classes.projectContent}>
         <div className={classes.titleAndIcons}>
@@ -22,6 +37,7 @@ const ProjectItem = (props) => {
           consectetur adipiscing elit, sed do eiusmod tempor made using React,
           Node.js and Djungo
         </p>
+        <h3>Made using React and Firebase</h3>
       </div>
     </div>
   );
--- src/Components/Projects.js
+++ src/Components/Projects.js
@@ -1,16 +1,27 @@
+import { useContext } from "react";
+
+import DarkLightContext from "../store/dark-light-context";
 import ProjectItem from "./ProjectItem";
-import classes from './Projects.module.css';
+import classes from "./Projects.module.css";
 
 const Projects = () => {
-  return(
-    <section className={classes.projectsSection}>
+  const darkLightCtx = useContext(DarkLightContext);
+
+  const isDarkMode = darkLightCtx.isDarkMode;
+
+  const projectSectionClasses = isDarkMode
+    ? classes.projectsSectionDarkMode
+    : classes.projectSectionLightMode;
+    
+  return (
+    <section className={`${classes.projectsSection} ${projectSectionClasses}`}>
       <h1>A Few Projects I've Built</h1>
-      <ProjectItem/>
-      <ProjectItem/>
-      <ProjectItem/>
-      <ProjectItem/>
+      <ProjectItem />
+      <ProjectItem />
+      <ProjectItem />
+      <ProjectItem />
     </section>
-  )
+  );
 };
 
 export default Projects;
