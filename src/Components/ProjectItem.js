838829b86bb95bca25035fd01e411b41be298770
Alex Pritchard <alexbeet1234@gmail.com>
2022-02-05 10:16:20 +0800

    added image to projectItem

--- src/Components/ProjectItem.js
+++ src/Components/ProjectItem.js
@@ -1,5 +1,30 @@
-const ProjectItem = () => {
-  return <div></div>
+import { AiFillGithub } from "react-icons/ai";
+
+import classes from "./ProjectItem.module.css";
+
+const ProjectItem = (props) => {
+  return (
+    <div className={classes.projectContainer}>
+      <div className={classes.image}>
+        <img src="https://images.unsplash.com/photo-1643960987290-37971bf5f2bd?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1171&q=80" alt="project" />
+      </div>
+      <div className={classes.projectContent}>
+        <div className={classes.titleAndIcons}>
+          <h1>Project Title</h1>
+          <div className={classes.icons}>
+            <AiFillGithub />
+          </div>
+        </div>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+          eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
+          minim veniam, quis nostrud exercitation Lorem ipsum dolor sit amet,
+          consectetur adipiscing elit, sed do eiusmod tempor made using React,
+          Node.js and Djungo
+        </p>
+      </div>
+    </div>
+  );
 };
 
 export default ProjectItem;
--- src/Components/Projects.js
+++ src/Components/Projects.js
@@ -1,9 +1,12 @@
 import ProjectItem from "./ProjectItem";
-
+import classes from './Projects.module.css';
 
 const Projects = () => {
   return(
-    <ProjectItem/>
+    <section className={classes.projectsSection}>
+      <h1>A Few Projects I've Built</h1>
+      <ProjectItem/>
+    </section>
   )
 };
 
