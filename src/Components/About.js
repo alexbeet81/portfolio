3d59e57f8eeeafde0e87b50a32d9a54bcc138633
Alex Pritchard <alexbeet1234@gmail.com>
2022-02-08 16:26:45 +0800

    updated all copy

--- src/Components/About.js
+++ src/Components/About.js
@@ -1,6 +1,6 @@
 import React, { useContext } from "react";
 
-import profilePhoto from '../images/profile-photo.png';
+import profilePhoto from "../images/profile-photo.png";
 import DarkLightContext from "../store/dark-light-context";
 import classes from "./About.module.css";
 
@@ -16,17 +16,32 @@ const About = () => {
   return (
     <section className={`${classes.aboutSection} ${aboutSectionClasses}`}>
       <div id="work" className={classes.image}>
-        <img src={profilePhoto} alt="profile photo"/>
+        <img src={profilePhoto} alt="profile photo" />
       </div>
       <div>
         <h1>About Me</h1>
         <p>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-          eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
-          minim veniam, quis nostrud exercitation Lorem ipsum dolor sit amet,
-          consectetur adipiscing elit, sed do eiusmod tempor incididunt ut
-          labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud
-          exercitation
+          I printed my first “hello, world” over two years ago and have been
+          busy learning ever since.
+        </p>
+        <p>
+          Since completing a full-stack Bootcamp in July of last year, I have
+          been trying to cram as much information in my head as I can. Mainly
+          Udemy courses with a focus on React and Node.js. I am now trying to
+          put all this new information to good use. This portfolio is a product
+          of that; I built it with React and am hosting it on firebase.
+        </p>
+        <p>
+          I was also honored to be a teaching assistant for LeWagon Shanghai.
+        </p>
+        <p>
+          I am based in Beijing but will move home to the UK this July, where I
+          shall be eagerly looking for opportunities. Please,{" "}
+          <a href="mailto:alexbeet1234@gmail.com" target="_blank">
+            {" "}
+            get in touch
+          </a>{" "}
+          if you would like to chat.
         </p>
       </div>
     </section>
--- src/Components/Contact.js
+++ src/Components/Contact.js
@@ -21,12 +21,11 @@ const Contact = () => {
     >
       <h1>Get In Contact</h1>
       <p>
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
-        tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
-        veniam, quis nostrud exercitation Lorem ipsum dolor sit amet,
-        consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore
-        et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud
-        exercitation
+        I am currently on the lookout for new opportunities. I am interested in
+        both freelance and full-time positions. If you are looking to add
+        someone to your team, someone to help build your next web app, or if you
+        are just interested in working on a hobby project with me, please
+        get in touch.
       </p>
       <a href="mailto:alexbeet1234@gmail.com">
         <button type="button" className={`${classes.button} ${buttonClasses}`}>
