99d48fac66c375adf31f59b88dde03ae58d29be7
Alex Pritchard <alexbeet1234@gmail.com>
2022-02-10 12:40:09 +0800

    used useReducer to set state in app

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import classes from "./App.module.css";
-import React, { useContext } from "react";
+import React, { useContext, useEffect, useReducer } from "react";
 import DarkLightContext from "./store/dark-light-context";
 
 import Intro from "./Components/Intro";
@@ -11,20 +11,31 @@ import Projects from "./Components/Projects";
 import Contact from "./Components/Contact";
 import Footer from "./Components/Footer";
 
+const reducer = (state, action) => {
+  return { animationIsFinished: true };
+};
+
 function App() {
+  const [state, dispatch] = useReducer(reducer, { animationIsFinished: false });
+
   const darkLightCtx = useContext(DarkLightContext);
 
   const isDarkMode = darkLightCtx.isDarkMode;
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      console.log("time out");
+      dispatch();
+    }, 3000);
+
+    return () => clearTimeout(timer);
+  }, []);
 
   const backgroundClasses = isDarkMode
     ? classes.backgroundDark
     : classes.backgroundLight;
 
-  const introComponent = (
-    <Intro />
-  )
-
-  const mainComponent = (
+  const mainComponent = state.animationIsFinished ? (
     <div>
       {" "}
       <NavBar />
@@ -37,13 +48,11 @@ function App() {
       </main>
       <Footer />
     </div>
+  ) : (
+    <Intro />
   );
 
-  return (
-    <div className={backgroundClasses}>
-      {introComponent}
-    </div>
-  );
+  return <div className={backgroundClasses}>{mainComponent}</div>;
 }
 
 export default App;
