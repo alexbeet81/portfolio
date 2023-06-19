8897a014bf7e6b2d76acb60a4dc720b1db728871
Alex Pritchard <alexbeet1234@gmail.com>
2023-06-19 18:24:57 +0100

    update copy

--- src/Components/About.js
+++ src/Components/About.js
@@ -31,32 +31,24 @@ const About = () => {
         <div>
           <h1>About Me</h1>
           <p>
-            I printed my first “hello, world” over two years ago and have been
-            busy learning ever since.
+            Over the past year, I've gained valuable professional experience in the field of web development.
           </p>
           <p>
-            Since completing a full-stack Bootcamp in July of last year, I have
-            been trying to cram as much information in my head as I can. Mainly
-            Udemy courses with a focus on React and Node.js. I am now trying to
-            put all this new information to good use. This portfolio is a
-            product of that; I built it with React and am hosting it on
-            firebase.
+            Since graduating from a full-stack bootcamp, I've been thriving in a fast-paced startup environment, constantly expanding my knowledge and skill set. Every day presents new challenges that push me to my limits, and I embrace them wholeheartedly. With each coding task, I discover a deeper passion for the craft, and my enthusiasm only continues to grow.
           </p>
           <p>
-            I was also honored to be a teaching assistant for LeWagon Shanghai.
+            I had the incredible opportunity to contribute as a teaching assistant for LeWagon Shanghai, where I further honed my expertise and shared my passion for web development.
           </p>
           <p>
-            I am based in Beijing but will move home to the UK this July, where
-            I shall be eagerly looking for opportunities. Please,{" "}
+            Based on my newfound knowledge and experience, I am eagerly seeking new opportunities to apply my skills and contribute to exciting projects. If you would like to connect or discuss potential collaborations, please don't hesitate to{" "}
             <a
               href="mailto:alexbeet1234@gmail.com"
               target="_blank"
               rel="noopener noreferrer"
             >
               {" "}
-              get in touch
-            </a>{" "}
-            if you would like to chat.
+              reach out.
+            </a>
           </p>
         </div>
       </section>
