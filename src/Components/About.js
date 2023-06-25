32acda9e62cd3ffaaf543f371f9bfe66413ca12e
Alex Pritchard <alexbeet1234@gmail.com>
2023-06-25 09:30:15 +0100

    update about page

--- src/Components/About.js
+++ src/Components/About.js
@@ -34,13 +34,13 @@ const About = () => {
             Over the past year, I've gained valuable professional experience in the field of web development.
           </p>
           <p>
-            Since graduating from a full-stack bootcamp, I've been thriving in a fast-paced startup environment, constantly expanding my knowledge and skill set. Every day presents new challenges that push me to my limits, and I embrace them wholeheartedly. With each coding task, I discover a deeper passion for the craft, and my enthusiasm only continues to grow.
+            Since graduating from a full-stack bootcamp, I've been working in a fast-paced startup, constantly learning new. Every day presents new challenges that push me. The more I learn, the deeper my passion for development grows.
           </p>
           <p>
-            I had the incredible opportunity to contribute as a teaching assistant for LeWagon Shanghai, where I further honed my expertise and shared my passion for web development.
+            I had the incredible opportunity to contribute as a teaching assistant for LeWagon Shanghai, not only enabling me to pick up new skills, but also to share my interest in code with others.
           </p>
           <p>
-            Based on my newfound knowledge and experience, I am eagerly seeking new opportunities to apply my skills and contribute to exciting projects. If you would like to connect or discuss potential collaborations, please don't hesitate to{" "}
+          I am eagerly seeking new opportunities to apply my skills and contribute to exciting projects. If you would like to connect or discuss potential collaborations, please don't hesitate to{" "}
             <a
               href="mailto:alexbeet1234@gmail.com"
               target="_blank"
