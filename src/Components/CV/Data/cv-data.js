c2535a95a724d936b45640ea0587e4e0e3a684c7
Alex Pritchard <alexbeet1234@gmail.com>
2023-09-27 20:08:10 +0100

    add cv data

--- src/Components/CV/Data/cv-data.js
+++ src/Components/CV/Data/cv-data.js
@@ -1,104 +1,179 @@
 export const cvData = [
   {
-    header: "Teaching (2011-2021)",
+    header: "WEB DEVELOPMENT (2021 - PRESENT)",
     content: {
       description:
-        "Over the last decade, I've passionately taught various subjects ranging from mathematics to science. Through engaging teaching methodologies, I have aimed to inspire and empower young minds, showcasing the tangible applications of theoretical knowledge.",
+        "In just over two years, I've transitioned from a bootcamp student to a full-stack web developer, with a broad range of experience in e-commerce solutions, UI/UX design optimization, and cross-platform integration.",
       jobs: [
         {
-          jobTitle: "High School Mathematics Teacher",
-          dates: "dec 2011 - dec 2021",
+          jobTitle: "Junior Ruby on Rails Developer, Virtue Impact, Remote",
+          dates: "Sep 2022 - Sep 2023",
           jobDescription:
-            "As the lead of the math department, I drove curriculum development and played an instrumental role in student mentorship, focusing on diverse learning methods.",
+            "As one of two developers for Virtue, an ecommerce app integrated with Shopify, WooCommerce, and Magento, I had the privilege of working alongside Liam Houlahan, a seasoned professional with 11 years of experience. Under his mentorship, I gained holistic exposure, handling every aspect of the app—from front-end development to back-end, testing and customer interaction via Crisp chat. Over the past year, I have been integral in significant milestones:",
           bulletPoints: [
-            "Introduced modern teaching methods involving technology.",
-            "Prepared students for national level exams with a high pass rate.",
-            "Championed the introduction of advanced calculus to the curriculum.",
+            "Implemented and maintained front-end features: Including Virtue Admin, store Widgets and e-mails using a combination of Stimulus.js, Liquid, React Typescript and vanilla JS.",
+            "Integrated with WooCommerce: Broadened the app's reach and compatibility.",
+            "Created and maintained front-end and back-end unit and integration tests utilisting Cypress and Rspec.",
+            "Developed and maintained database using raw SQL for speed, while employing ActiveRecord for data integrity in a Ruby on Rails environment.",
+            "Identified a user need, subsequently developed and tested a new customer giving widget, resulting in increased donation rates through enhanced user experience.",
+            "Integrated user analytics: Adopted data-driven improvements by tracking user behaviour.",
+            "Produced app documentation: Provided detailed written and video guides, streamlining future updates and troubleshooting.",
           ],
         },
         {
-          jobTitle: "Physics Teacher",
-          dates: "jun 2015 - dec 2021",
+          jobTitle:
+            "Lead Teacher / Teaching Assistant, LeWagon, Shanghai, China",
+          dates: "Sep 2021 - July 2022",
           jobDescription:
-            "Teaching physics, I emphasized hands-on learning, leveraging experiments to explain complex concepts.",
+            "After the bootcamp, I took on a mentoring role, utilising my problem-solving skills to guide incoming students and further deepen my technical proficiency. My adept communication and teaching abilities led to my appointment as the Lead Teacher for the Ruby module, where I taught foundational programming concepts and adapted my teaching methods to cater to diverse learning styles.",
+          bulletPoints: [],
+        },
+        {
+          jobTitle:
+            "Student on 24-week intensive Web development Bootcamp, LeWagon, Shanghai, China",
+          dates: "Feb 2021 - July 2021",
+          jobDescription:
+            "While working full-time as a middle school teacher, I undertook a 24-week intensive program during evenings and weekends. Motivated by a desire to bolster my IT skills and pivot towards a new career, the course covered foundational Ruby, software modelling, databases with SQL, front-end development, and the Rails framework. My learning journey peaked with the creation of an AirBnb replica and a comprehensive 4-week final project.",
+          bulletPoints: [],
+        },
+      ],
+    },
+  },
+  {
+    header: "TEACHING (2012 - 2022)",
+    content: {
+      description:
+        "Driven by a desire to travel and explore new cultures, I completed a 120-hour TEFL certificate and embarked on an educational journey to China. Starting in a business English training centre, I progressed from a classroom teacher to a training manager, eventually taking on a leadership role at a renowned middle school.",
+      jobs: [
+        {
+          jobTitle:
+            "Middle Subject Leader, Beijing 21st Century International School, Beijing, China",
+          dates: "Sep 2017 - July 2021",
+          jobDescription: "",
           bulletPoints: [
-            "Organized monthly field trips to science museums and factories.",
-            "Co-authored a physics textbook now used across the district.",
-            "Promoted the use of interactive apps and simulations in teaching.",
+            "Curated the English curriculum for grades 7 - 9 at one of China's premier private schools.",
+            "Identified students facing challenges and established an after-school class to help them align with the main class syllabus.",
+            "Initiated an after-school film class, empowering students to produce films which they then submitted to a competition.",
           ],
         },
         {
-          jobTitle: "STEM Coordinator",
-          dates: "jan 2018 - dec 2021",
-          jobDescription:
-            "Managed and coordinated all STEM activities in school, ensuring a comprehensive approach to integrated learning.",
+          jobTitle: "Training Manager, Oxford Centeriver, Beijing, China",
+          dates: "Feb 2015 - Aug 2017",
+          jobDescription: "",
           bulletPoints: [
-            "Launched an annual STEM week, inviting industry professionals for talks.",
-            "Oversaw the school's robotics team, leading them to a national victory.",
-            "Initiated partnerships with local tech companies for internships.",
+            "Crafted bespoke lesson plans for employees of Fortune 500 companies across mainland China.",
+            "Managed and trained teachers, ensuring rigorous quality standards and implementing a cohesive pedagogy.",
+            "Delivered personalised one-to-one lessons to leading CEOs and senior business executives.",
+          ],
+        },
+        {
+          jobTitle: "Teacher, Elite Learning, Beijing, China",
+          dates: "Apr 2012 - Jan 2015",
+          jobDescription:
+            "Initiated and consulted on the redesign of the core curriculum alongside a colleague. This collaborative effort led to a more accurate English learning process, enhancing the overall educational experience.",
+          bulletPoints: [],
+        },
+        {
+          jobTitle: "120-hour TEFL Teaching Certificate, London",
+          dates: "Jan 2012 - Mar 2012",
+          jobDescription: "",
+          bulletPoints: [],
+        },
+      ],
+    },
+  },
+  {
+    header: "PERSONAL SABBATICAL (2010 - 2011)",
+    content: {
+      description:
+        "Seeking a change, I ventured to Arizona and immersed myself in conservation work. While I had plans to extend my stay, the unexpected terminal illness of a family member brought me back to the UK. During this challenging period, I secured employment through a temp agency, gaining experience in various factory production lines. The demanding shift work taught me adaptability and resilience.",
+      jobs: [
+        {
+          jobTitle:
+            "Conservationist, American Conservation Experience, Flagstaff, AZ",
+          dates: "Sep 2010 - Nov 2010",
+          jobDescription: "",
+          bulletPoints: [],
+        },
+        {
+          jobTitle: "Temp, HR GO Recruitment, Wells, Somerset",
+          dates: "Jan 2011 - Dec 2011",
+          jobDescription: "",
+          bulletPoints: [],
+        },
+      ],
+    },
+  },
+  {
+    header: "TV AND FILM (2006 - 2010)",
+    content: {
+      description:
+        "After landing a runner role on the movie ‘Hot Fuzz’, I seized the opportunity to transition into a full-time position in London. My journey took me through one of the world’s premier television and film production companies, eventually leading me to delve into post-production.",
+      jobs: [
+        {
+          jobTitle: "Client Services, Suite Post Production Ltd, London",
+          dates: "April 2008 - Sep 2010",
+          jobDescription:
+            "Served as the primary liaison for both crew and production at Suite, a 24-hour facility with 20 editing studios. Suite is renowned for hosting post-production of some of the UK's flagship television shows, including 'The Office', 'The Inbetweeners', and 'Mock the Week'.",
+          bulletPoints: [
+            "Acted as the main point of contact for the crew and performers, facilitating smooth collaboration and communication, provided top-tier hospitality, ensuring a positive experience for all visitors.",
+            "Demonstrated strong interpersonal skills, building rapport and trust with team members and guests alike, all while working to a tight deadline and staying positive.",
+          ],
+        },
+        {
+          jobTitle: "Production Assistant, Big Talk Productions, London",
+          dates: "May 2006 - April 2008",
+          jobDescription:
+            "Big Talk is responsible for some of the UK’s biggest films; ‘Shaun of the Dead’, ‘Hot Fuzz’ and ‘Scott Pilgrim vs The World’. I served as a vital support to producers, directors, and artists. My role spanned a range of tasks, demonstrating versatility, adaptability, and the ability to manage high-pressure situations with tight deadlines. An integral part of ensuring a smooth and productive office environment.",
+          bulletPoints: [
+            "Designed and implemented a comprehensive archival system for essential files, scripts, and movie memorabilia, ensuring streamlined retrieval.",
+            "Oversaw office management ensuring an efficient and organised workspace.",
+            "Played a pivotal role in hiring and managing runners, both in-office and on set.",
+            "Filmed and edited casting sessions, ensuring high-quality footage for directorial review.",
           ],
         },
       ],
     },
   },
   {
-    header: "Research & Development (2005-2010)",
+    header: "POST UNIVERSITY (2005 - 2006)",
     content: {
       description:
-        "My five years in R&D were focused on pushing the boundaries of knowledge in applied mathematics. Collaborating with experts, I aimed to merge theory with practical solutions, addressing global challenges.",
+        "While searching to start a career in Film and Television, I worked at a busy gastro pub, mastering quick-paced tasks and customer interactions.",
       jobs: [
         {
-          jobTitle: "Lead Researcher",
-          dates: "jan 2005 - nov 2010",
-          jobDescription:
-            "Guided the team towards innovative research areas, ensuring projects were purposeful and impactful.",
-          bulletPoints: [
-            "Published papers in top-tier academic journals.",
-            "Secured grants for pioneering projects in applied math.",
-            "Facilitated academic-industry collaborations for real-world applications.",
-          ],
-        },
-        {
-          jobTitle: "Post-Doctoral Fellow",
-          dates: "jan 2006 - dec 2007",
-          jobDescription:
-            "Worked on niche areas of applied math, mentoring graduate students and publishing groundbreaking papers.",
-          bulletPoints: [
-            "Introduced novel methodologies for problem-solving.",
-            "Collaborated with global experts on joint research.",
-            "Delivered lectures and presentations at international conferences.",
-          ],
+          jobTitle: "Front of House Staff - The Fountain Inn, Wells, Somerset",
+          dates: "Jan 2005 - May 2006",
+          jobDescription: "",
+          bulletPoints: [],
         },
       ],
     },
   },
   {
-    header: "Freelance Educational Consultant (2002-2004)",
+    header: "EDUCATION (1999 - 2004)",
     content: {
       description:
-        "As a consultant, I collaborated with educational institutions worldwide, offering insights and strategies to enhance their academic frameworks and teaching methodologies.",
+        "After finishing school, I back-packed down Australia's eastern coast, ending in Sydney where I enrolled in a fine arts foundation. Back in the UK, I earned a degree in Animation and further honed my skills through a 3-month intensive course with Aardman Animations' Arril Johnson.",
       jobs: [
         {
-          jobTitle: "Curriculum Advisor",
-          dates: "mar 2002 - dec 2003",
-          jobDescription:
-            "Worked with institutions to update and innovate their curriculum, ensuring it remained relevant and engaging.",
-          bulletPoints: [
-            "Introduced modern pedagogical methods in 10+ institutions.",
-            "Tailored curricula to local needs while maintaining global standards.",
-            "Trained educators in the latest teaching methods and technologies.",
-          ],
+          jobTitle:
+            "University of the West of England - Bristol School of Animation",
+          dates: "Oct 2004 - Dec 2004",
+          degjobDescriptionee: "",
+          bulletPoints: [],
         },
         {
-          jobTitle: "Educational Policy Consultant",
-          dates: "jan 2003 - dec 2004",
-          jobDescription:
-            "Advised on policy-making, ensuring educational reforms were beneficial and in line with global best practices.",
-          bulletPoints: [
-            "Played a key role in drafting education policies in two states.",
-            "Lobbied for increased focus on STEM in early education.",
-            "Promoted inclusive education, ensuring accessibility for all.",
-          ],
+          jobTitle: "University of Plymouth, Plymouth, Devon",
+          dates: "Sep 2001 - July 2004",
+          degjobDescriptionee: "BA (Hons) Animation and Creative Media - 3rd",
+          bulletPoints: [],
+        },
+        {
+          jobTitle: "Sydney College of Art and Design, Sydney, Australia",
+          dates: "July 1999 - Sep 2001",
+          degjobDescriptionee: "",
+          bulletPoints: [],
         },
       ],
     },
